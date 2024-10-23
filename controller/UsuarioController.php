9be647a4087474c17ecf223cb6c5c3647ffedb43
Moraes076 <felipe.desmoraes@gmail.com>
2024-10-23 11:56:21 -0300

    Update (login)

---
+++ controller/UsuarioController.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once './models/Usuario.php';
+
+class UsuarioController {
+
+    protected $model;
+
+    function __construct() {
+        $this->model = new UsuarioModel;
+    }
+
+    function login($data) {
+        $result = $this->model->login($data);
+
+        if ($result == false) {
+            $_SESSION['login_error'] = 'Usuário ou senha inválidos';
+        } else {
+            unset($_SESSION['login_error']);
+        }    
+
+        header('Location: ./index.php');
+    }
+}
